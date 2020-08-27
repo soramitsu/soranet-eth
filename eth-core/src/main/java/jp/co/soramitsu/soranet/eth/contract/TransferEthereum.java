68f031121a6951c38cb7e1fd1d999cb63e21f4d0
Mingela <freekilluau@gmail.com>
2020-08-27 13:36:22 +0300

    Copyrights

--- eth-core/src/main/java/jp/co/soramitsu/soranet/eth/contract/TransferEthereum.java
+++ eth-core/src/main/java/jp/co/soramitsu/soranet/eth/contract/TransferEthereum.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package jp.co.soramitsu.soranet.eth.contract;
 
 import java.math.BigInteger;
